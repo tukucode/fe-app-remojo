6430ae10cac313af82c402f19e917d7296b7e93e
tomimandalap <tommymandalaputra@gmail.com>
2024-02-17 15:26:33 +0700

    added small media query for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer id="footer--rental--mobil" className="text-s3">
+    <footer id="footer--rental--mobil">
       Rental Mobil Jogja - Copyright 2024
     </footer>
   );
