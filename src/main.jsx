07f39b0dcf08337925a551f1249cedc7e13124e0
tomimandalap <tommymandalaputra@gmail.com>
2024-02-17 10:39:45 +0700

    added global.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Store from "./store/index.js";
 import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
