6cc74fd2610634b414c9fd51e448ced55a4395ef
tomimandalap <tommymandalaputra@gmail.com>
2024-02-17 10:07:47 +0700

    implementation layouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// LAYOUTS
+import LayoutDashboard from "./layouts/Dashboard";
+import LayoutLanding from "./layouts/Landing";
+
+// PAGES
+import Home from "./pages/Index";
+import Admin from "./pages/admin/Index";
+
 export default function App() {
   return (
-    <>
-      <h2>Hello World</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* DASHBOARD */}
+        <Route Component={LayoutDashboard}>
+          <Route index path="/admin" element={<Admin />} />
+        </Route>
+
+        {/* LANDING */}
+        <Route Component={LayoutLanding}>
+          <Route index path="/" Component={Home} />
+        </Route>
+
+        {/* PAGE NOT FOUND */}
+        <Route path="/*" element={<h1>Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
