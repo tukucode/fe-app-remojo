54daf895dca7b1f83f8f9afb29ed6f521a45c246
tomimandalap <tommymandalaputra@gmail.com>
2024-02-17 10:56:08 +0700

    added class text-dark

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -8,11 +8,11 @@ export default function FormAuth(props) {
   return (
     <Card id="card__form__auth" className="rounded-0 shadow-sm">
       <Card.Body className="body__form">
-        <Card.Title className="text-center text-h3 title__form">
+        <Card.Title className="text-center text-dark text-h3 title__form">
           {title}
         </Card.Title>
 
-        <Card.Subtitle className="text-p4 subtitle__form">
+        <Card.Subtitle className="text-dark text-p4 subtitle__form">
           {subtitle}
         </Card.Subtitle>
 
