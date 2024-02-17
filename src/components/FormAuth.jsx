6533665f77910a8089c194c97036c2bc7e9670a4
tomimandalap <tommymandalaputra@gmail.com>
2024-02-17 10:53:30 +0700

    added component FormAuth

---
+++ src/components/FormAuth.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { Card } from "react-bootstrap";
+import "../assets/css/FormAuth.css";
+
+export default function FormAuth(props) {
+  let { children, title = "Title", subtitle = "Subtitle" } = props;
+
+  return (
+    <Card id="card__form__auth" className="rounded-0 shadow-sm">
+      <Card.Body className="body__form">
+        <Card.Title className="text-center text-h3 title__form">
+          {title}
+        </Card.Title>
+
+        <Card.Subtitle className="text-p4 subtitle__form">
+          {subtitle}
+        </Card.Subtitle>
+
+        {children}
+      </Card.Body>
+    </Card>
+  );
+}
