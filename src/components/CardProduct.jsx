11180066c9e0f6b8fd7e310b350b8164ce218a39
tomimandalap <tommymandalaputra@gmail.com>
2024-04-19 12:09:59 +0700

    feat: added rounded-0 on image card

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,11 +1,19 @@
-import { Card, Button } from 'react-bootstrap'
-import { formatIDR } from "../utils/formater.js"
+/* eslint-disable react/prop-types */
+import { Card, Button } from "react-bootstrap";
+import { formatIDR } from "../utils/formater.js";
 
-// eslint-disable-next-line react/prop-types
-export default function CardProduct({ product = {}, titleBtn = 'Edit', onClickBtn = () => { } }) {
+export default function CardProduct({
+  product = {},
+  titleBtn = "Edit",
+  onClickBtn = () => { },
+}) {
   return (
     <Card className="rounded-0">
-      <Card.Img variant="top" src={product.detail_storage.secure_url} />
+      <Card.Img
+        variant="top"
+        src={product.detail_storage.secure_url}
+        className="rounded-0"
+      />
       <Card.Body className="text-center">
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{formatIDR(product.price)}</Card.Text>
@@ -15,5 +23,5 @@ export default function CardProduct({ product = {}, titleBtn = 'Edit', onClickBt
         </Button>
       </Card.Body>
     </Card>
-  )
+  );
 }
