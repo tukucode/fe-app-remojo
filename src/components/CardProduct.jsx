29da5721db5ed996b3527d4a62991e1e3727151f
tomimandalap <tommymandalaputra@gmail.com>
2024-04-19 10:43:20 +0700

    feat: create component CardProduct

---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,19 @@
+import { Card, Button } from 'react-bootstrap'
+import { formatIDR } from "../utils/formater.js"
+
+// eslint-disable-next-line react/prop-types
+export default function CardProduct({ product = {}, titleBtn = 'Edit', onClickBtn = () => { } }) {
+  return (
+    <Card className="rounded-0">
+      <Card.Img variant="top" src={product.detail_storage.secure_url} />
+      <Card.Body className="text-center">
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>{formatIDR(product.price)}</Card.Text>
+
+        <Button variant="dark" className="rounded-0 w-50" onClick={onClickBtn}>
+          {titleBtn}
+        </Button>
+      </Card.Body>
+    </Card>
+  )
+}
--- src/components/admin/data-mobil/ListProduct.jsx
+++ src/components/admin/data-mobil/ListProduct.jsx
@@ -1,28 +1,24 @@
-import { Card, Row, Col, Button } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import EmptyProduct from "../../EmptyProduct";
-
-import { formatIDR } from "../../../utils/formater.js";
+import CardProduct from "../../CardProduct";
 
 // eslint-disable-next-line react/prop-types
 export default function ListProduct({ dataProduct = [] }) {
+
+  function onEditProduct(detailProduct) {
+    console.log('ini ', detailProduct);
+  }
+
+
   const divStyle = { marginTop: "2.875rem" };
   if (!dataProduct.length) return <EmptyProduct />;
 
   return (
     <div style={divStyle}>
       <Row>
-        {dataProduct.map((product, index) => (
+        {dataProduct.map((detailProduct, index) => (
           <Col key={`col-card-product-${index + 1}`} lg="3">
-            <Card className="rounded-0">
-              <Card.Img variant="top" src={product.detail_storage.secure_url} />
-              <Card.Body className="text-center">
-                <Card.Title>{product.name}</Card.Title>
-                <Card.Text>{formatIDR(product.price)}</Card.Text>
-                <Button variant="dark" className="rounded-0 w-50">
-                  Edit
-                </Button>
-              </Card.Body>
-            </Card>
+            <CardProduct product={detailProduct} onClickBtn={() => onEditProduct(detailProduct)} />
           </Col>
         ))}
       </Row>
