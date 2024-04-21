df89242097c5626f0162d0ccb8b10114138644ad
tomimandalap <tommymandalaputra@gmail.com>
2024-04-21 15:54:47 +0700

    feat: implementation ux produt maintenance

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -9,15 +9,39 @@ export default function CardProduct(props) {
     aspectRatio: "1/1",
     objectFit: "contain",
     objectPosition: "center",
+    opacity: product.deleted_at ? "0.3" : "1",
   };
-  return (
-    <Card className="rounded-0 h-100">
+
+  let element;
+  if (product && product.deleted_at) {
+    element = (
+      <div className="position-relative d-flex justify-content-center align-items-center">
+        <Card.Text className="position-absolute">
+          Kendaraan dalam perbaikan.
+        </Card.Text>
+        <Card.Img
+          variant="top"
+          src={product.detail_storage.secure_url}
+          className="rounded-0"
+          style={styleImg}
+        />
+      </div>
+    );
+  } else {
+    element = (
       <Card.Img
         variant="top"
         src={product.detail_storage.secure_url}
         className="rounded-0"
         style={styleImg}
       />
+    );
+  }
+
+  return (
+    <Card className="rounded-0 h-100">
+      {element}
+
       <Card.Body className="text-center">
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{formatIDR(product.price)}</Card.Text>
