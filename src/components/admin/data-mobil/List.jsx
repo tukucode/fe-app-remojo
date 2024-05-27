9a4b2d2ef9006e892b29a6477f81fd3aa9e4c42b
tomimandalap <tommymandalaputra@gmail.com>
2024-05-27 10:38:48 +0700

    fix: added responsive card

--- src/components/admin/data-mobil/List.jsx
+++ src/components/admin/data-mobil/List.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function ListProduct({ dataProduct = [] }) {
   const navigateTo = useNavigate();
   function goToEdit(_id) {
-    navigateTo(`/admin/data-mobil/edit/${_id}`)
+    navigateTo(`/admin/data-mobil/edit/${_id}`);
   }
 
   const divStyle = { margin: "2.875rem 0" };
@@ -17,7 +17,13 @@ export default function ListProduct({ dataProduct = [] }) {
     <div style={divStyle}>
       <Row className="g-3">
         {dataProduct.map((detailProduct, index) => (
-          <Col key={`col-card-product-${index + 1}`} lg="3">
+          <Col
+            key={`col-card-product-${index + 1}`}
+            xl="3"
+            lg="4"
+            md="6"
+            xs="12"
+          >
             <CardProduct
               product={detailProduct}
               onClickBtn={() => goToEdit(detailProduct._id)}
