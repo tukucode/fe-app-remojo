72abaf52bd2ca86d7b72bb108c485566e2b7db9d
tomimandalap <tommymandalaputra@gmail.com>
2024-05-27 09:58:15 +0700

    feat: pairing api product customer

---
+++ src/components/customer/beranda/Cars.jsx
@@ -0,0 +1,64 @@
+import { Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import CardProduct from "../../card/Product";
+import EmptyProduct from "../../empty/Product";
+
+// eslint-disable-next-line react/prop-types
+export default function CustomerBerandaCars({ cars = [] }) {
+  const navigateTo = useNavigate();
+  function handleCheckout(product_id) {
+    navigateTo(`/rental-mobil/checkout/${product_id}`);
+  }
+
+  let element;
+  if (!cars.length) {
+    element = (
+      <div className="text-center mb-5">
+        <EmptyProduct />
+        <h4>Mohon maaf,</h4>
+        <h6>Saat ini kendaraan tidak tersedia.</h6>
+      </div>
+    );
+  } else {
+    element = (
+      <>
+        <Row className="mt-4 mb-5 g-3">
+          {cars.map((car, index) => (
+            <Col
+              key={`card-car-beranda-${index}}`}
+              xl="3"
+              lg="4"
+              md="6"
+              xs="12"
+            >
+              <CardProduct
+                product={car}
+                titleBtn="Sewa sekarang"
+                onClickBtn={() => handleCheckout(car._id)}
+              />
+            </Col>
+          ))}
+        </Row>
+
+        <div className="text-center mb-5">
+          <Button
+            variant="outline-dark"
+            className="rounded-0"
+            onClick={() => navigateTo("/rental-mobil")}
+          >
+            Tampilkan lebih banyak
+          </Button>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3 className="text-h3">Pilih kendaraan terbaikmu</h3>
+
+      {element}
+    </>
+  );
+}
--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -1,15 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { toast } from "react-toastify";
 
 import CustomerBerandaHero from "../components/customer/beranda/Hero";
 import CustomerBerandaInfo from "../components/customer/beranda/Info";
+import CustomerBerandaCars from "../components/customer/beranda/Cars";
+
+import moment from "moment";
+import useAxios from "../hooks/useAxios";
+import useLoading from "../hooks/useLoading";
 
 export default function Home() {
+  const axios = useAxios();
+  const { showLoading, hideLoading } = useLoading();
+
+  const [cars, setCars] = useState([]);
+  const params = {
+    q: "",
+    start_date: moment().endOf("day").format("YYYY-MM-DD"),
+    end_date: moment().endOf("day").format("YYYY-MM-DD"),
+    page: 1,
+    per_page: 8,
+  };
+
+  useEffect(() => {
+    showLoading();
+
+    axios
+      .get("api/v1/customer/product", { params })
+      .then((response) => {
+        setCars(response.data.data);
+      })
+      .catch((error) => {
+        let { message, data } = error.response.data;
+
+        if (data) {
+          for (let resError of data.errors) {
+            toast.error(`${resError.message} of ${resError.path}`);
+          }
+
+          return;
+        }
+
+        toast.error(message);
+      })
+      .finally(() => {
+        hideLoading();
+      });
+  }, []);
+
   return (
     <section id="beranda">
       <CustomerBerandaHero />
 
       <Container>
         <CustomerBerandaInfo />
+
+        <CustomerBerandaCars cars={cars} />
       </Container>
     </section>
   );
