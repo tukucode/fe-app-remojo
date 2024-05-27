b01f7ee67ae0c4b64716809ebf24011514254cee
tomimandalap <tommymandalaputra@gmail.com>
2024-05-27 21:21:06 +0700

    feat: added router guard custoner

--- src/layouts/Landing.jsx
+++ src/layouts/Landing.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import { Container, Navbar, Button, Nav } from "react-bootstrap";
-import { Outlet, useLocation, NavLink, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import {
+  Outlet,
+  useLocation,
+  NavLink,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 import useLoading from "../hooks/useLoading";
@@ -14,6 +20,19 @@ export default function LayoutLanding() {
   let cssShowMenu = show ? "d-block" : "d-none";
   function onShowMenu() {
     setShow(!show);
+  }
+
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+  function onLogout() {
+    // REMOVE TOKEN AT LOCAL STORAGE
+    localStorage.removeItem("token");
+
+    // REASSIGN TOKEN AT REDUCER USER
+    dispatch({ type: "SET_TOKEN", value: null });
+
+    // NAGITATION TO PAGE LOGIN
+    navigateTo("/login");
   }
 
   const { pathname } = useLocation();
@@ -60,7 +79,7 @@ export default function LayoutLanding() {
             <Nav className="me-auto">
               <NavLink
                 to="/beranda"
-                className="inactive ms-md-2"
+                className="inactive ms-md-3"
                 active-class-name="active"
               >
                 Beranda
@@ -68,23 +87,60 @@ export default function LayoutLanding() {
 
               <NavLink
                 to="/rental-mobil"
-                className="inactive ms-md-2"
+                className="inactive ms-md-3"
                 active-class-name="active"
               >
                 Rental Mobil
               </NavLink>
+
+              {token ? (
+                <>
+                  <NavLink
+                    to="/daftar-sewa"
+                    className="inactive ms-md-3"
+                    active-class-name="active"
+                  >
+                    Daftar Sewa
+                  </NavLink>
+
+                  <NavLink
+                    to="/profile"
+                    className="inactive ms-md-3"
+                    active-class-name="active"
+                  >
+                    Profile Anda
+                  </NavLink>
+                </>
+              ) : null}
             </Nav>
 
-            <Button
-              variant="outline-dark"
-              className="rounded-0 my-md-0 my-2 me-3"
-            >
-              Daftar
-            </Button>
+            {token ? (
+              <Button
+                variant="outline-danger"
+                className="rounded-0 my-md-0 my-2"
+                onClick={onLogout}
+              >
+                Keluar
+              </Button>
+            ) : (
+              <>
+                <Button
+                  variant="outline-dark"
+                  className="rounded-0 my-md-0 my-2 me-3"
+                  onClick={() => navigateTo("/register")}
+                >
+                  Daftar
+                </Button>
 
-            <Button variant="dark" className="rounded-0 my-md-0 my-2">
-              Masuk
-            </Button>
+                <Button
+                  variant="dark"
+                  className="rounded-0 my-md-0 my-2"
+                  onClick={() => navigateTo("/login")}
+                >
+                  Masuk
+                </Button>
+              </>
+            )}
           </Navbar.Collapse>
         </Container>
       </Navbar>
