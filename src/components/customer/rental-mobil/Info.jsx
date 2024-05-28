faf600e19c2645e4fbe6572b1f88d2f293196d10
tomimandalap <tommymandalaputra@gmail.com>
2024-05-28 09:52:18 +0700

    feat: pairing checkout with midtrans

---
+++ src/components/customer/rental-mobil/Info.jsx
@@ -0,0 +1,161 @@
+/* eslint-disable react/prop-types */
+import { useFormik } from "formik";
+import { Row, Col, Image, Form, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+import * as Yup from "yup";
+import moment from "moment";
+
+import { formatIDR } from "../../../utils/formater";
+import InputDate from "../../InputDate";
+
+import { jwtDecode } from "jwt-decode";
+
+export default function RentalMobilInfo({ detail, onSubmitForm = () => {} }) {
+  const styleImg = {
+    aspectRatio: "1/1",
+    objectFit: "contain",
+    objectPosition: "center",
+    width: "100%",
+    height: "100%",
+    maxHeight: "400px",
+    border: "1px solid #ccc",
+  };
+
+  const { _id: product_id, detail_storage, name, price, description } = detail;
+  const toDay = moment().format("YYYY-MM-DD");
+
+  const schema = Yup.object({
+    start_date: Yup.string().required("Field is required"),
+    end_date: Yup.string().required("Field is required"),
+  });
+
+  const initialForm = {
+    start_date: "",
+    end_date: "",
+  };
+
+  const { token } = useSelector((store) => store.user);
+
+  const Formik = useFormik({
+    initialValues: initialForm,
+    validationSchema: schema,
+    onSubmit: (values) => {
+      let { user_id } = jwtDecode(token);
+
+      let payload = {
+        rental_duration: { ...values },
+        user_id,
+        product_ids: [product_id],
+      };
+
+      onSubmitForm(payload);
+    },
+  });
+
+  return (
+    <Row className="g-md-5 g-1">
+      <Col lg="4" md="5" sm="12" xs="12" className="h-100">
+        <Image
+          src={detail_storage.secure_url}
+          alt={detail_storage.public_id}
+          style={styleImg}
+          loading="lazy"
+        />
+      </Col>
+
+      <Col lg="8" md="7" sm="12" xs="12" className="h-100">
+        <h2 className="text-h2">{name}</h2>
+
+        <p className="text-p1">{formatIDR(price ?? 0)}</p>
+
+        <hr />
+        <p className="text-p3">{description}</p>
+        <hr />
+
+        <h4 className="text-h4 mb-3">Informasi Penting</h4>
+
+        <h5 className="text-s4">Setelah Anda pesan</h5>
+        <ul>
+          <li>
+            Penyedia akan menghubungi pengemudi melalui WhatsApp untuk meminta
+            foto beberapa dokumen wajib.
+          </li>
+        </ul>
+
+        <h5 className="text-s4">Saat pengambilan</h5>
+        <ul>
+          <li>
+            Bawa KTP, SIM A, dan dokumen-dokumen lain yang dibutuhkan oleh
+            penyedia rental.
+          </li>
+          <li>Tunjukan bukti pemesanan pelunasan sewa.</li>
+          <li>
+            Saat Anda bertemu dengan staf rental, cek kondisi mobil dengan staf.
+          </li>
+          <li>Setelah itu, baca dan tanda tangan perjanjian rental.</li>
+        </ul>
+
+        <h5 className="text-s4">Kebijakan refund & Perubahan jadwal sewa</h5>
+        <ul>
+          <li>
+            Mengenai pengembalian dana/perubahan jadwal dapat dilakukan minimal
+            24 jam sebelum waktu sewa dilakukan.
+          </li>
+          <li>
+            Apabila telah memasuki waktu atau durasi penyewaan maka akan kami
+            anggap tidak dapat direfund kembali.
+          </li>
+          <li>
+            Perihal pengembalian dana (refund) dapat menghubungi pihak rental
+            dengan syarat ketentuan yang berlaku.
+          </li>
+          <li>
+            Perubahan jadwal sewa dapat melakukan dengan klik ubah jadwal sewa
+            di menu Daftar Sewa.
+          </li>
+        </ul>
+
+        <Form onSubmit={Formik.handleSubmit}>
+          <Row className="g-xl-3 g-2">
+            <Col xl="3" lg="4" md="6" xs="12">
+              <InputDate
+                name="start_date"
+                placeholder="Tanggal mulai sewa"
+                minDate={toDay}
+                valueInput={Formik.values.start_date}
+                onChangeDate={Formik.handleChange}
+                isInvalid={!!Formik.errors.start_date}
+              >
+                <Form.Control.Feedback type="invalid">
+                  {Formik.errors.start_date}
+                </Form.Control.Feedback>
+              </InputDate>
+            </Col>
+
+            <Col xl="3" lg="4" md="6" xs="12">
+              <InputDate
+                name="end_date"
+                placeholder="Tanggal selesai sewa"
+                minDate={toDay}
+                valueInput={Formik.values.end_date}
+                onChangeDate={Formik.handleChange}
+                isInvalid={!!Formik.errors.end_date}
+              >
+                <Form.Control.Feedback type="invalid">
+                  {Formik.errors.end_date}
+                </Form.Control.Feedback>
+              </InputDate>
+            </Col>
+
+            <Col xl="3" lg="4">
+              <Button type="submit" variant="dark" className="rounded-0 w-100">
+                Bayar sekarang
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Col>
+    </Row>
+  );
+}
--- src/pages/rental-mobil/Checkout.jsx
+++ src/pages/rental-mobil/Checkout.jsx
@@ -1,11 +1,115 @@
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Container } from "react-bootstrap";
+
+import useLoading from "../../hooks/useLoading";
+import useAxios from "../../hooks/useAxios";
+
+import NavBreadcrumb from "../../components/NavBreadcrumb";
+import RentalMobilInfo from "../../components/customer/rental-mobil/Info";
 
 export default function RentalMobilCheckout() {
   const { product_id } = useParams();
+  let navList = [
+    {
+      to: "/rental-mobil",
+      title: "Rental Mobil",
+      isActive: false,
+    },
+    {
+      to: "/rental-mobil",
+      title: "Checkout",
+      isActive: true,
+    },
+    {
+      to: `/rental-mobil/checkout/${product_id}`,
+      title: product_id,
+      isActive: true,
+    },
+  ];
+
+  const axios = useAxios();
+  const navigateTo = useNavigate();
+  const { showLoading, hideLoading } = useLoading();
+
+  function handleSubmit(values) {
+    showLoading();
+
+    axios
+      .post("api/v1/transaction/checkout", values)
+      .then((response) => {
+        console.log("ini", response.data.data);
+        let { token } = response.data.data;
+
+        window.snap.pay(token, {
+          onSuccess: function (result) {
+            toast.success(result);
+            navigateTo("/daftar-sewa");
+          },
+          onPending: function (result) {
+            toast.info(result);
+          },
+          onError: function (result) {
+            toast.error(result);
+          },
+          onClose: function () {
+            // CLOSE POPUP
+            toast.info("You closed the popup without finishing the payment");
+            navigateTo("/daftar-sewa");
+          },
+        });
+      })
+      .catch((error) => {
+        let { message, data } = error.response.data;
+
+        if (data) {
+          for (let resError of data.errors) {
+            toast.error(`${resError.message} of ${resError.path}`);
+          }
+
+          return;
+        }
+
+        toast.error(message);
+      })
+      .finally(() => {
+        hideLoading();
+      });
+  }
+
+  const [detail, setDetail] = useState(null);
+  useEffect(() => {
+    showLoading();
+
+    axios
+      .get(`api/v1/customer/product/${product_id}`)
+      .then((response) => {
+        setDetail(response.data.data);
+      })
+      .catch((error) => {
+        let { message } = error.response.data;
+        toast.error(message);
+      })
+      .finally(() => {
+        hideLoading();
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <h1>Checkout {JSON.stringify(product_id)}</h1>
-    </>
+    <section
+      id="dedtail-checkout"
+      className="min-vh-100"
+      style={{ margin: "2.875rem auto" }}
+    >
+      <Container>
+        <NavBreadcrumb navList={navList} />
+
+        {detail ? (
+          <RentalMobilInfo detail={detail} onSubmitForm={handleSubmit} />
+        ) : null}
+      </Container>
+    </section>
   );
 }
