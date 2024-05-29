2ada0a5fd054031f9fd961ee2897c67018893608
tomimandalap <tommymandalaputra@gmail.com>
2024-05-29 13:15:32 +0700

    feat: pairing api update user

---
+++ src/components/FormProfile.jsx
@@ -0,0 +1,201 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react";
+import { useFormik } from "formik";
+import { Row, Col, Image, Form, Button } from "react-bootstrap";
+
+import * as Yup from "yup";
+
+export default function FormProfile({ detail, onSubmitForm = () => {} }) {
+  let { detail_storage } = detail;
+
+  const styleImg = {
+    aspectRatio: "1/1",
+    objectFit: "contain",
+    objectPosition: "center",
+    width: "100%",
+    height: "100%",
+    maxHeight: "400px",
+    border: "1px solid #ccc",
+  };
+
+  let secure_url = detail_storage
+    ? detail_storage.secure_url
+    : "/images/placeholder.png";
+
+  const schema = Yup.object({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    first_name: Yup.string()
+      .required("First name is required")
+      .max(20, "First name must not exceed 20 characters"),
+    phone: Yup.string()
+      .required("Phone is required")
+      .matches(
+        /^(\+62|62)?[\s-]?0?8[1-9]{1}\d{1}[\s-]?\d{4}[\s-]?\d{2,5}$/,
+        "Phone number invalid. (+62|62|0) 8xxxxxxx"
+      ),
+    // image: Yup.mixed().required("Image is required"),
+  });
+
+  const initialForm = {
+    email: "",
+    first_name: "",
+    last_name: "",
+    phone: "",
+    image: null,
+  };
+
+  const Formik = useFormik({
+    initialValues: initialForm,
+    validationSchema: schema,
+    onSubmit: (values) => onSubmitForm(values),
+  });
+
+  function handleChangeFile(event) {
+    // ASSIGN VALUE TO INITIAL FORM IMAGE
+    let file = event.target.files[0];
+    Formik.values.image = file;
+
+    // TOUCH FORM INPUT FILE IMAGE
+    Formik.setFieldTouched("image", true);
+  }
+
+  // RESET FORM
+  const refInputFile = useRef();
+  function handleReset() {
+    // reset input file
+    refInputFile.current.value = null;
+
+    // reset formik
+    Formik.resetForm({
+      values: initialForm,
+      touched: {},
+    });
+
+    window.location.reload();
+  }
+
+  useEffect(() => {
+    let { email, first_name, last_name, phone } = detail;
+    Formik.values.email = email;
+    Formik.values.first_name = first_name;
+    Formik.values.last_name = last_name;
+    Formik.values.phone = phone;
+    Formik.values.image = null;
+  }, []);
+
+  return (
+    <Row className="g-md-5 g-sm-4 g-3">
+      <Col lg="3" md="4" sm="5">
+        <Image
+          src={secure_url}
+          alt="user_profile"
+          style={styleImg}
+          loading="lazy"
+        />
+      </Col>
+
+      <Col lg="9" md="8" sm="7">
+        <h4 className="text-s2">Informasi data diri</h4>
+
+        <Form onSubmit={Formik.handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="email"
+              name="email"
+              value={Formik.values.email}
+              onChange={Formik.handleChange}
+              isInvalid={!!Formik.errors.email}
+              disabled
+              placeholder="Email"
+              className="rounded-0"
+            />
+            <Form.Control.Feedback type="invalid">
+              {Formik.errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              name="first_name"
+              value={Formik.values.first_name}
+              onChange={Formik.handleChange}
+              isInvalid={!!Formik.errors.first_name}
+              placeholder="Nama depan"
+              maxLength="30"
+              autoComplete="off"
+              className="rounded-0"
+            />
+            <Form.Control.Feedback type="invalid">
+              {Formik.errors.first_name}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              name="last_name"
+              value={Formik.values.last_name}
+              onChange={Formik.handleChange}
+              isInvalid={!!Formik.errors.last_name}
+              placeholder="Nama belakang"
+              maxLength="30"
+              autoComplete="off"
+              className="rounded-0"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="tel"
+              name="phone"
+              value={Formik.values.phone}
+              onChange={Formik.handleChange}
+              isInvalid={!!Formik.errors.phone}
+              placeholder="Nomor handphone"
+              maxLength="16"
+              autoComplete="off"
+              className="rounded-0"
+            />
+            <Form.Control.Feedback type="invalid">
+              {Formik.errors.phone}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Control
+              ref={refInputFile}
+              type="file"
+              id="product-image"
+              name="image"
+              accept=".jpg, .jpeg, .png"
+              onChange={handleChangeFile}
+              isInvalid={!!Formik.errors.image}
+              className="rounded-0"
+            />
+
+            <Form.Control.Feedback type="invalid">
+              {Formik.errors.image}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Button
+            type="button"
+            variant="primary"
+            className="rounded-0 me-3"
+            onClick={handleReset}
+          >
+            Reset
+          </Button>
+
+          <Button type="submit" variant="success" className="rounded-0">
+            Perbaharui data diri
+          </Button>
+        </Form>
+      </Col>
+    </Row>
+  );
+}
--- src/pages/profile/Index.jsx
+++ src/pages/profile/Index.jsx
@@ -1,7 +1,96 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { jwtDecode } from "jwt-decode";
+import { toast } from "react-toastify";
+
+import NavBreadcrumb from "../../components/NavBreadcrumb";
+import FormProfile from "../../components/FormProfile";
+
+import useAxios from "../../hooks/useAxios";
+import useLoading from "../../hooks/useLoading";
+
+let navList = [
+  {
+    to: "/profile",
+    title: "Profile Anda",
+    isActive: true,
+  },
+];
+
 export default function ProfileCustomer() {
+  const axios = useAxios();
+  const { showLoading, hideLoading } = useLoading();
+  const { token } = useSelector((store) => store.user);
+  const { user_id } = jwtDecode(token);
+
+  const [detail, setDetail] = useState(null);
+
+  function handleSubmit(values) {
+    const formData = new FormData();
+
+    for (const key in values) {
+      if (values[key] !== null) {
+        formData.append(key, values[key]);
+      }
+    }
+
+    showLoading();
+
+    axios
+      .put(`api/v1/user/${user_id}`, formData)
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        let { message, data } = error.response.data;
+
+        if (data) {
+          for (let resError of data.errors) {
+            toast.error(`${resError.message} of ${resError.path}`);
+          }
+
+          return;
+        }
+
+        toast.error(message);
+      })
+      .finally(() => {
+        hideLoading();
+      });
+  }
+
+  function getDetailUser() {
+    showLoading();
+
+    axios
+      .get(`api/v1/user/${user_id}`)
+      .then((response) => {
+        setDetail(response.data.data);
+      })
+      .catch((error) => {
+        const { message } = error.response;
+        toast.error(message);
+      })
+      .finally(() => {
+        hideLoading();
+      });
+  }
+
+  useEffect(() => {
+    getDetailUser();
+  }, []);
+
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <section id="user-profile" className="min-vh-100">
+      <Container style={{ margin: "2.875rem auto" }}>
+        <NavBreadcrumb navList={navList} />
+
+        {detail ? (
+          <FormProfile detail={detail} onSubmitForm={handleSubmit} />
+        ) : null}
+      </Container>
+    </section>
   );
 }
